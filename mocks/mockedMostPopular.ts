c5f53b630a9f33f38aad939a51284a631f2b0f4e
Stanislav Kelemen <kelemen.work@gmail.com>
2021-03-01 12:08:23 +0200

    make most popular route consistent

--- mocks/mockedMostPopular.ts
+++ mocks/mockedMostPopular.ts
@@ -1,7 +1,6 @@
-import * as fakerHelper from './helpers';
+import products from './mockedProducts';
 
 const ITEMS_NUM = 6;
-
 interface MostPopularItem {
   id: string,
   name: string,
@@ -9,17 +8,14 @@ interface MostPopularItem {
     value: number,
     currency: string,
   },
+  amount: {
+    value: number,
+    unit: string
+  },
   mainImage: string
 }
 
-const mostPopularItem = (faker: Faker.FakerStatic): MostPopularItem => ({
-  id: faker.random.uuid(),
-  name: faker.name.title(),
-  price: {
-    value: fakerHelper.randNum(10, 1000),
-    currency: 'USD'
-  },
-  mainImage: fakerHelper.randomImage()
-});
-
-export default fakerHelper.timesObj(ITEMS_NUM, mostPopularItem);
+export default Object.values(products).slice(0, ITEMS_NUM)
+  .map (({ id, name, price, amount, mainImage, }): MostPopularItem => ({
+    id, name, price, amount, mainImage
+  }));
